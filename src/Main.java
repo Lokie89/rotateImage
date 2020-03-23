09cbd93db30715c7c58cdb4c96a45c1ca1c6d0ca
Lokie89 <traeuman@gmail.com>
2020-03-23 22:20:25 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         try {
-            List<File> fileList = new FileOutput().getFileList("C:\\Users\\amoeba\\Desktop\\www");
-            String outputPath = "C:\\Users\\amoeba\\Desktop\\www2";
+            String filePath = "C:\\Users\\traeu\\Desktop\\www";
+            List<File> fileList = new FileOutput().getFileList(filePath);
             for (int i = 0; i < fileList.size(); i++) {
                 File file = fileList.get(i);
-                new ImageOutput().writeImage(file, outputPath);
+                new ImageOutput().writeImage(file, filePath + "\\modified");
             }
         } catch (IOException e) {
             e.printStackTrace();
